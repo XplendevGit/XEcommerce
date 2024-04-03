6efdc3574bc1ec02aefec208025dacb004c96b43
Luis Rivera <l.riveraa664@hotmail.com>
2024-04-03 18:50:30 -0300

    Updates

--- components/portada/Portada.jsx
+++ components/portada/Portada.jsx
@@ -25,8 +25,8 @@ const Portada = () => {
     }, []);
 
   return (
-    <div className="w-full h-screen bg-fondoprimary container">
-{/* DIV Central */}
+    <div className="w-full max-h-screen bg-fondoprimary container">
+    {/* DIV Central */}
       <div className={`w-full h-full flex lg:py-24 md:py-10 py-4 ${style['contenedor-portada']}` }>
       {/* DIV 1 */}
         <div className={` ${style['portada-1']}` }>
@@ -35,7 +35,7 @@ const Portada = () => {
              {/* Textos */}
              <h2 className="text-xl md:text-2xl text-primary">Profesional</h2>
              <h2 className="text-3xl md:text-5xl text-white" style={{ textShadow: '0px 0px 8px #252734' }}>EXCLUSIVE</h2>
-             <h1 className="xl:tracking-[0.8em] lg:tracking-[0.3em] md:tracking-[0.2em] tracking-[0.04em]" style={{ textShadow: '8px 4px 4px rgba(0,0,0,0.5)' }}>EXPERIENCES</h1>
+             <h1 className="xl:tracking-[0.8em] lg:tracking-[0.3em] md:tracking-[0.2em]" style={{ textShadow: '8px 4px 4px rgba(0,0,0,0.5)' }}>EXPERIENCES</h1>
 
             {/* Boton de Grupo */}
             <div className="flex items-center gap-4 pt-4 mx-auto sm:mx-0">
@@ -53,7 +53,7 @@ const Portada = () => {
                 {/* Imagen */}
                 
                 <div 
-            className="flex w-auto h-[480px] md:w-[590px] md:h-[580px] xl:w-[790px] xl:h-[780px] xl:pb-12 ">
+            className="flex w-auto h-[340px] md:w-[590px] md:h-[580px] xl:w-[790px] xl:h-[780px] xl:pb-12 ">
                 {loading ? (
                        <div className="inset-0 w-full flex items-center justify-center bg-opacity-50">
                             <Image src={'https://i.postimg.cc/sfK4D4mg/loading.gif'} width={640} height={640} alt="Loading" className="w-32 h-32" /> {/* Ajusta el tamaño según sea necesario */}
